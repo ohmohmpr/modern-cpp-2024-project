05e0914cff991a5907bafa318406bcb1b19e7858
Panyawat Rattana <nanoic_ohm@hotmail.com>
2024-07-11 03:06:01 +0200

    Change: eigen source build to install lib

--- prerequisites.sh
+++ prerequisites.sh
@@ -4,8 +4,4 @@ chmod +x open3d-devel-linux-x86_64-cxx11-abi-0.18.0.tar.xz
 tar xf open3d-devel-linux-x86_64-cxx11-abi-0.18.0.tar.xz --transform 's/^open3d-devel-linux-x86_64-cxx11-abi-0.18.0/open3d/' -C 3rdparty
 rm -rf open3d-devel-linux-x86_64-cxx11-abi-0.18.0.tar.xz
 
-# download eigen
-wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
-chmod +x eigen-3.4.0.tar.gz
-tar xf eigen-3.4.0.tar.gz --transform 's/^eigen-3.4.0/eigen/' -C 3rdparty
-rm -rf eigen-3.4.0.tar.gz
+sudo apt-get install libeigen3-dev
